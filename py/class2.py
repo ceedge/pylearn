5a2f331708e046cfd00afc7470c0b121b336f1dc
ceedge <37727159+ceedge@users.noreply.github.com>
2022-02-13 14:59:49 -0500

    class2
    new class for operators

---
+++ py/class2.py
@@ -0,0 +1,17 @@
+'''
+Created on Feb 13, 2022
+
+@author: Joker
+'''
+# using true and false with operators 
+from pickle import TRUE, FALSE
+
+c=10
+d=5
+if c>10 and d>1:
+    print("it worked")
+False and True
+False
+if c>=10 and d>1:
+    print("it worked")
+    
