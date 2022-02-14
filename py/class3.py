3b694d87cf9796c950c6d56e8f4e37c8205fefe2
ceedge <37727159+ceedge@users.noreply.github.com>
2022-02-13 20:25:09 -0500

    Create class3.py

---
+++ py/class3.py
@@ -0,0 +1,26 @@
+'''
+Created on Feb 13, 2022
+
+@author: Joker
+'''
+
+
+# in this we are creating list and other project to be named later
+our_list= [27,46,-5,17,99]
+print(our_list)
+
+print(type(our_list))
+
+jack=["a", "b","c",1,2,3,"do","rey", "me", True,False]
+
+print(jack[6])
+
+print(jack[0:3])
+
+listr=[1,2,[3,4,5],6,7,8]
+
+print(listr[2][0])
+
+mtable= [[1,2,3],[4,5,6],[7,8,9]]
+
+print(mtable[1])
