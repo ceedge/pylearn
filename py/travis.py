bd935d10edf80d2b3042a5159672f4f92153265b
ceedge <37727159+ceedge@users.noreply.github.com>
2022-02-20 00:33:49 -0500

    Update travis.py

--- py/travis.py
+++ py/travis.py
@@ -7,3 +7,12 @@ Created on Feb 17, 2022
 known_user = ["Alice", "Bob", "Dan", "Fred", "Harry", "Jim"]
 
 print(len(known_user))
+
+while True:
+    print("Hi my name is Travis")
+    name= input("What is your name? : ").strip().capitalize()
+    if name in known_user:
+        print("name recognised")
+    else:
+        print("name NOT  recognised")
+    
