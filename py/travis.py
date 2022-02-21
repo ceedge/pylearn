cc67461ee48ff356d26c161f92f76d19e4f62e0a
ceedge <37727159+ceedge@users.noreply.github.com>
2022-02-21 02:44:41 -0500

    Update travis.py

--- py/travis.py
+++ py/travis.py
@@ -13,14 +13,21 @@ while True:
     name= input("What is your name? : ").strip().capitalize()
     if name in known_user:
         print("Hello {}!".format(name))
-        remove= input("Would you like to be removed from the system (y/n)?").lower()
+        remove= input("Would you like to be removed from the system (y/n)?").strip().lower()
         if remove== "y":
             print(known_user)
             known_user.remove(name)
             print(known_user)
+        elif remove=="n":
+            print("Not a problem, i didn't want  you to leave anyway!")
             
     else:
-        print("I dont't think we have met you yet{}".format(name))
-        add_me=input("Would you like to be added to the system (y/n)? :")
-        
+        print("I dont't think we have met you yet {}".format(name))
+        add_me=input("Would you like to be added to the system (y/n)? :").strip().lower()
+        if add_me=="y":
+            print(known_user)
+            known_user.append(name)
+            print(known_user)
+        elif add_me =="n":
+            print("Ok, you won't be added")
     
