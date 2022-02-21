bc49c165457259c73f9ff4a89923499fb3ad6e8f
ceedge <37727159+ceedge@users.noreply.github.com>
2022-02-21 00:39:38 -0500

    Update travis.py

--- py/travis.py
+++ py/travis.py
@@ -12,7 +12,15 @@ while True:
     print("Hi my name is Travis")
     name= input("What is your name? : ").strip().capitalize()
     if name in known_user:
-        print("name recognised")
+        print("Hello {}!".format(name))
+        remove= input("Would you like to be removed from the system (y/n)?").lower()
+        if remove== "y":
+            print(known_user)
+            known_user.remove(name)
+            print(known_user)
+            
     else:
-        print("name NOT  recognised")
+        print("I dont't think we have met you yet{}".format(name))
+        add_me=input("Would you like to be added to the system (y/n)? :")
+        
     
