974875523731609dab267232fe3bfe4554fafa35
ceedge <37727159+ceedge@users.noreply.github.com>
2022-04-02 23:12:16 -0400

    function
    creating new function

---
+++ py/function_time.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 23, 2022
+
+@author: Joker
+'''
+def add(x,y):
+    return x+y
+
+print(add(5,10))
+
+ans=add(100,100)
+
+print(ans)
+
+
+
+def rev(text):
+    return text[::-1]
+
---
+++ py/pack_unpack.py
@@ -0,0 +1,24 @@
+'''
+Created on Apr 2, 2022
+
+@author: Joker
+'''
+num=[1,2,3,4,5]
+
+def add(x,y):
+    return x+y
+add(10,10)
+def adds(*num):
+    total=0
+    for number in num:
+        total=total+number
+    return(total)
+a=adds(1,2,3,4,5,6,7,8,9)
+print(a)
+print(type(a))
+
+def about(name,age,likes):
+    sent="meet {} they are {} years old and they like {}".format(name,age,likes)
+    return sent
+dict={"name":"bob", "age":23,"likes":"python"}
+print(about(**dict))
