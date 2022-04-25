7b5b92dc362945913dc695af72b8e7a00ffc0847
ceedge <37727159+ceedge@users.noreply.github.com>
2022-04-24 22:07:46 -0400

    Update money.py

--- py/money.py
+++ py/money.py
@@ -3,8 +3,12 @@ Created on Apr 3, 2022
 
 @author: Joker
 '''
-
-
+import random
+class coin:
+    def __init__(self,rare=False, clean=True):
+        self.is_rare=rare
+        self.is_clean=clean
+        
 class Pound:
     def __init__(self, rare=False):
         
@@ -16,15 +20,21 @@ class Pound:
             self.value=1.00
              
         
-        
-        
         self.color="gold"
         self.num_edges=1
         self.diameter=22.5#mm
         self.thickness=3.15#mm
         self.heads=True
+    def __del__(self):
+        print("Coin Spent!")
     def rust(self):
         self.color="greenish"
+    def clean(self):
+        self.color="gold"
+    def flip(self):
+        head_op=[True,False]
+        choice= random.choice(head_op)
+        self.heads=choice
         
     
 
